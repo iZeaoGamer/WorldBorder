f9eae8fa5cc6355e8cc4b3a9bf435d163bc8dbf2
iZeaoGamer <jameshayward14@icloud.com>
2020-04-07 13:13:41 +0100

    Merge master branch to dev (#6)
    * Remove MultiWorld check
    
    * Add Dependency for MultiWorld
    
    * Add spawn-location option
    
    * Added spawn-location option to config.yml
    
    * getLevelByName() function
    
    * Fixed syntax error
    
    * version bump
    
    * poggit y u lying to me? there's no syntax errors.
    
    * Trigger a build
    
    * !== v1.1.2 UPDATE !==
    
    - Fixed undefined issues.
    - Fixed syntax error. (If poggit will recognize that I changed the code. ;-;)
    - Added new option: spawn-location, description is in the config.yml file.
    
    * Update .poggit.yml
    
    * What's with the two #'s?
    
    * Add "dev" tag
    
    * Revert
    
    * !== WorldBorder BUG FIXES !==
    
    - Fixed coordinates from not being numeric when using spawn-cordinates, and leaving spawn-location and def-level-spawn false.
    - Added a check function: isSameFile to ensure whether or not def-level-spawn and spawn-location are both set to true. If they are, it'll return an error message. This check is in place so there's no conflictions between the two config options, since this could lead to potential errors, or one that works, and the other one doesn't. This way is better so we don't need to see the outcome of the two of the same files colliding together.
    - Added a cordinates check to ensure X, Y, and Z are numeric, and not colliding with strings. (Tested to work).
    
    * Release v1.1.2
    
    This commit releases, and fixes potential Syntax errors.
    
    * Fix typo
    
    This commit fixes typo with coordinates, since that's the proper wording for it apparently. So I'll just keep it as that for now.
    
    * !== 1.2.0 UPDATE AVAILABLE !==
    
    - Added Commands support!
    - Added forms API support by jojoe77777
    - Added a new config option: open-type, as self explained in the config.yml file.
    - You can now use the config.yml file in-game!
    - You can now use forms to make this plugin customisable in-game!
    
    * !== FORMS UPDATE !==
    
    - Plugin no longer requires Form API to run.
    - Moved everything Forms related to FormManager (AKA FormManagement).
    - Added new classes: CustomForm and SimpleForm.
    - Added instances functions from Main Class.
    - Clean-up in WorldBorder Command class.
    - Remove Erroring UI's for now, may add this back in the near future.
    
    * !== MORE UPDATES !==
    
    - Added a new check before executing commanded-options to ensure the data 1 and the other option aren't both true. Yes, I'm referring to def-level-spawn and spawn-location.
    
    * Fixed message
    
    This would've outcome lots of errors.
    
    * Update functions to PHP 7.x
    
    Since we now use ints, strings, etc before the argument took place since PHP 7 I believe. So I updated the code to suit its needs.
    I also removed the commented line which wasn’t needed.
    
    * Update features
    
    * Fixed #3
    
    This commit fixes Location class not found. My bad!
    
    * !== v1.2.1 UPDATE ,==
    
    * Fix #4
    
    * Update plugin.yml
    
    * Remove Quotation marks (Untested)
    
    I’m keeping the sections without quotation marks.
    
    * Do not use PHP Stan
    
    This commit should fix the many errors with PHP Stan.
    
    * !== Add basic API methods !==
    
    The following functions are now introduced:
    - isInWorld()
    - isInRange()
    - setRange()
    - ReloadConfig()
    - getPluginConfig() - Uses internal.
    - Add a bit more API’s as todo lists.
    
    
    - Added function typehints, and descriptions.
    
    * Bump Plugin Version to 1.2.2
    
    This is because of “API methods” update, which should be released soon after 1.2.1 has been published to poggit.
    
    * API methods, which most do not have.
    
    This commit adds and updates The ReadME.MD, which contains something new that most WorldBorder plugins do not have - API METHODS.
    
    * Testing purposes (Will lead to errors)
    
    * Update .poggit.yml
    
    * Update .poggit.yml
    
    Co-authored-by: XenialDan <thebigsmilexd@gmail.com>

--- src/iZeaoGamer/WorldBorder/Main.php
+++ src/iZeaoGamer/WorldBorder/Main.php
@@ -9,6 +9,10 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 use pocketmine\level\Position;
+use pocketmine\level\Location;
+
+use iZeaoGamer\WorldBorder\utils\FormManager;
+use iZeaoGamer\WorldBorder\commands\WorldBorderCommand;
 
 class Main extends PluginBase implements Listener{
     private $x;
@@ -22,6 +26,8 @@ class Main extends PluginBase implements Listener{
     private $unsafeBlocks;
     private $level;
     public $config;
+     /** @var  Main $instance */
+     private static $instance;
 
     const safeBlocks = [0, 6, 8, 9, 27, 30, 31, 32, 37,
         38, 39, 40, 50, 59, 63, 64, 65,
@@ -31,26 +37,89 @@ class Main extends PluginBase implements Listener{
     const unsafeBlocks = [10, 11, 51, 81];
     
 public function onEnable(): void{
+    self::$instance = $this;
+    $this->formManager = new FormManager($this);
+
     
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 if(!is_file($this->getDataFolder() . "config.yml")){
     $this->saveDefaultConfig();
 }
 $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
-
+$this->getServer()->getCommandMap()->register("worldborder", new WorldBorderCommand($this));
+  if($this->isSameFile()){
+    $this->getLogger()->critical("There is a config error whilst loading the plugin.");
+    $this->getLogger()->error("def-level-spawn and spawn-location options cannot both be set to true in WorldBorder config.yml file on line 26 and line 30.");
+    $this->getServer()->getPluginManager()->disablePlugin($this);
+}
 if (!is_dir($this->getDataFolder())) {
     @mkdir($this->getDataFolder());
     }
-	 if($this->isPocketMineMP()){
-    $this->multiworld = $this->getServer()->getPluginManager()->getPlugin("MultiWorld");
-    if(!$this->multiworld){
-        $this->getLogger()->error("MultiWorld is required to install this plugin. Plugin disabled.");
-        $this->getServer()->getPluginManager()->disablePlugin($this);
+}
+	//todo be able to change safe, and unsafe block ids.
+
+     /**
+     * @return Main $plugin
+     */
+    public static function getInstance(): Main {
+        return self::$instance;
     }
+	/**
+	* Checks whether or not BOTH options are the same.
+	* @return bool
+	*/
+public function isSameFile() : bool{
+    return ($this->config->get("def-level-spawn") and $this->config->get("spawn-location"));
 }
+	/**
+	* Checks to ensure the worldborder is compatible with certain worlds, as configured in the config.yml file.
+	* @param Player $player
+	* @return bool
+	*/
+public function isInWorld(Player $player): bool{
+return (in_array($player->getLevel()->getFolderName(), $this->config->get("worlds")));
 }
-public function isPocketMineMP(){
-    return ($this->getServer()->getName() === "PocketMine-MP");
+	
+/**
+* Checks for if the position of the range is near or inside the specific range distance.
+* Player: Checks for the player name, and to recognize whether or not the player is in the range.
+* Position: Checks for the position of the range (More likely the spawnpoint or the spawn position (/setworldspawn, /setspawn, or otherwise.)
+* @param Player $player
+* @param Position $spawn
+* @return bool
+*/
+public function isInRange(Player $player, Position $spawn): bool{
+return ($spawn->distance($player) >= $this->config->get("range"));
+}
+	
+/**
+* Sets the range via the config.
+* @param int $range
+*/
+public function setRange(int $range){
+$this->config->set("range", $range);
+$this->config->save();
+}
+	
+/**
+* Reloads the config file, can be used by other plugins if wanting to reload their configurations.
+* @param Config
+* @param bool $save
+*/
+public function reloadConfig(Config $config, bool $save = true){
+	if($save){
+$config->save();
+	}
+$config->reload();
+}
+	
+/**
+* Returns the default config.yml used by this plugin.
+* This function will not work by other plugins. So this function is internal, and should only be used by this plugin.
+* @return Config
+*/
+public function getPluginConfig(): Config{
+return $this->config;
 }
 public function Boarder(PlayerMoveEvent $event){
   
@@ -58,7 +127,15 @@ public function Boarder(PlayerMoveEvent $event){
     if($this->config->get("def-level-spawn")){
         $spawn = $this->getServer()->getDefaultLevel()->getSpawnLocation();
     }else{
-        $spawn = new Vector3($this->config->get("spawn-coordinates")); //todo implement multiworld support
+	    if($this->config->get("spawn-location")){
+		    $spawn = $event->getPlayer()->getLevel()->getSpawnLocation();
+		    }else{
+                    $cords = explode(", ", $this->config->get("coordinates"));
+                    $x = $cords[0];
+                    $y = $cords[1];
+                    $z = $cords[2];
+        $spawn = new Vector3($x, $y, $z);
+    }
     }
 	 $player = $event->getPlayer();
 		 if($spawn->distance($player) >= $this->config->get("range")){
@@ -69,13 +146,13 @@ public function Boarder(PlayerMoveEvent $event){
 			  $player->sendMessage(TextFormat::colorize($this->config->get("border-message")));
 		 }
     }
-}
+    }
 
 															/**
      * @param Location $location
      * @return Vector3
      */
-    public function correctPosition($location) : Vector3 {
+    public function correctPosition(Location $location) : Vector3 {
         $knockback = 4.0;
         $x = $location->getX();
         $z = $location->getZ();
@@ -110,7 +187,7 @@ public function Boarder(PlayerMoveEvent $event){
      * @param int $z
      * @return int
      */
-    private function findSafeY(Level $level, $x, $y, $z) : int {
+    private function findSafeY(Level $level, int $x, int $y, int $z) : int {
         $top = $level->getHeightMap($x, $z) - 2;
         $bottom = 1;
         for($y1 = $y, $y2 = $y; ($y1 > $bottom) or ($y2 < $top); $y1--, $y2++){
@@ -130,7 +207,7 @@ public function Boarder(PlayerMoveEvent $event){
      * @param int $z
      * @return bool
      */
-    private function isSafe(Level $level, $x, $y, $z) : bool{
+    private function isSafe(Level $level, int $x, int $y, int $z) : bool{
         $safe = in_array($level->getBlockIdAt($x, $y, $z), self::safeBlocks) && in_array($level->getBlockIdAt($x, $y + 1, $z), self::safeBlocks);
         if(!$safe) return $safe;
         $below = $level->getBlockIdAt($x, $y - 1, $z);
---
+++ src/iZeaoGamer/WorldBorder/commands/WorldBorderCommand.php
@@ -0,0 +1,136 @@
+<?php 
+namespace iZeaoGamer\WorldBorder\commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat;
+use iZeaoGamer\WorldBorder\form\CustomForm;
+use iZeaoGamer\WorldBorder\form\SimpleForm;
+use iZeaoGamer\WorldBorder\Main;
+
+class WorldBorderCommand extends Command{
+    public function __construct(Main $plugin){
+        parent::__consturct("worldborder");
+        $this->setPermission("worldborder.command"); //todo make this configurable
+        $this->setDescription("Customise World Borders in-game!");
+        $this->setUsage("/worldborder <command>"); 
+        $this->setAliases(["wb"]); //todo make configurable.
+        $this->plugin = $plugin;
+    }
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
+        if(!$this->testPermission($sender)){
+            return true;
+        }
+        if(!$sender instanceof Player){
+            $sender->sendMessage("Use this command in-game!"); //todo make this configurable.
+            return true;
+        }
+            if($this->plugin->getConfig()->get("open-type") === "forms" or $this->plugin->getConfig()->get("open-type") === "both"){
+                $form = new SimpleForm("World Border", "Select action");
+                $form->id = 0;
+                $form->setContent("Customise World Border in-game!");
+                $form->addButton("Worlds");
+                $form->addButton("Range");
+                $form->addButton("Teleport");
+                $form->addButton("Default Level Spawn");
+                $form->addButton("Spawn Location");
+                $form->addButton("Coordinates");
+                $player->sendForm($form);
+
+            }elseif($this->plugin->getConfig()->get("open-type") === "command" or $this->plugin->getConfig()->get("open-type") === "both"){
+                if(!isset($args[0])){
+                $sender->sendMessage(TextFormat::colorize("&5&lWorldBorder &6Help Commands"));
+                $sender->sendMessage(TextFormat::colorize("&7/$commandLabel world <string> - &bMake WorldBorders work in a specific world."));
+                $sender->sendMessage(TextFormat::colorize("&7/$commandLabel range <int> - &bEdit how far a worldborder should be (In blocks)"));
+                $sender->sendMessage(TextFormat::colorize("&7/$commandLabel teleport <bool> - &bControls whether or not you get teleported to a safe location once you've reached the world border."));
+                $sender->sendMessage(TextFormat::colorize("&7/$commandLabel def-level-spawn <bool> - &bControls whether or not the range depends on the default level spawnpoint."));
+                $sender->sendMessage(TextFormat::colorize("&7/$commandLabel spawn-location <bool> - &bControls wether or not the range depends on the spawn-location (/setworldspawn)"));
+                $sender->sendMessage(TextFormat::colorize("&7/$commandLabel coordinates <int: X> <int: Y> <int: Z> - &bSets the coordinates to where the range begins from. (Must set def-level-spawn and spawn-location to false)."));
+                return true;
+                }
+            if($args[0] === "world"){
+                if(!isset($args[1])){
+                    $sender->sendMessage(TextFormat::colorize("&cYou didn't enter the worlds you wanted the world border to work in. Please use: /$commandLabel $args[0] <string: worlds>"));
+                    return true;
+                }
+                $this->plugin->getConfig()->set("worlds", implode("\n-", $args));
+            $this->plugin->getConfig()->save();
+            foreach($this->plugin->getConfig()->get("worlds") as $worlds){
+            $sender->sendMessage(TextFormat::colorize("&5You have set the Worlds to be set as the following: &6" . $worlds));
+            }
+            return true;
+        }
+        if($args[0] === "range"){
+            if(!isset($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cUse: /$commandLabel range <int>"));
+                return true;
+            }
+            if(is_string($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cArgument 1 must return a int. String given."));
+                return true;
+            }
+            $this->plugin->getConfig()->set("range", (int)$args[1]);
+            $this->plugin->getConfig()->save();
+            $sender->sendMessage(TextFormat::colorize("&5You have set the range to be as &6" . $args[1]));
+            return true;
+        }
+        if($args[0] === "teleport"){
+            if(!isset($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cUse: /$commandLabel teleport <bool>"));
+                return true;
+            }  
+            if(!is_bool($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cArgument 1 must be a bool, invalid argument given."));
+                return true;
+            }
+            $this->plugin->getConfig()->set("teleport", (bool)$args[1]);
+            $this->plugin->getConfig()->save();
+            $sender->sendMessage(TextFormat::colorize("&5You have set the teleportation to &6" . (bool)$args[1]));
+            return true;
+        }
+        if($args[0] === "def-level-spawn"){
+            if(!isset($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cUse: /$commandLabel def-level-spawn <bool>"));
+                return true;
+            }  
+            if(!is_bool($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cArgument 1 must be a bool, invalid argument given."));
+                return true;
+            }
+            $this->plugin->getConfig()->set("def-level-spawn", (bool)$args[1]);
+            $this->plugin->getConfig()->save();
+            $sender->sendMessage(TextFormat::colorize("&5You have set the def-level-spawn to &6" . (bool)$args[1]));
+            return true;
+        }
+        if($args[0] === "spawn-location"){
+            if(!isset($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cUse: /$commandLabel spawn-location <bool>"));
+                return true;
+            }  
+            if(!is_bool($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cArgument 1 must be a bool, invalid argument given."));
+                return true;
+            }
+            $this->plugin->getConfig()->set("spawn-location", (bool)$args[1]);
+            $this->plugin->getConfig()->save();
+            $sender->sendMessage(TextFormat::colorize("&5You have set the spawn-location to &6" . (bool)$args[1]));
+            return true;
+        }
+        if($args[0] === "coordinates"){
+            if(!isset($args[3])){
+                $sender->sendMessage(TextFormat::colorize("&cUse: /$commandLabel coordinates <int: X> <int: Y> <int: Z>"));
+                return true;
+            }
+            //todo implement checks to ensure coordinates aren't being handled via strings AKA letters.   
+            $this->plugin->getConfig()->set("coordinates", (int)$args[1], (int)$args[2], (int)$args[3]);
+            $this->plugin->getConfig()->save();
+            $sender->sendMessage(TextFormat::colorize("&5Your coordinates have been set successfully. The coordinates have been set at: &6" . $args[1] . ", " . $args[2] . ", and " . $args[3]));
+            return true;
+        }
+
+
+}
+
+
+}
+}
